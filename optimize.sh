16ce96dd00738a50eb1c5a439ac8c662ae39095b
luca020400 <luca.stefani.ge1@gmail.com>
2015-03-24 13:28:16 +0100

    More VARS

--- optimize.sh
+++ optimize.sh
@@ -2,19 +2,22 @@
 # APK Optimization Tool created by luca020400 and modified by Pizza_Dox
 
 #vars
-ver=1.0
+ver="1.0"
+apktool="java -jar bin/apktool.jar"
+signapk="java -jar bin/signapk.jar bin/testkey.x509.pem bin/testkey.pk8"
+zipalign="./bin/zipalign"
 
 function optimize_apk(){
 for apk in `ls *.apk | sed "s/.apk//"`; do
-    java -jar bin/apktool.jar d -s $apk.apk >/dev/null 2>&1
+    $apktool d -s $apk.apk >/dev/null 2>&1
         for png in `find $1 | grep png`; do
 	    ./bin/pngquant $apk/$png >/dev/null 2>&1
         done
-    java -jar bin/apktool.jar b -f $apk >/dev/null 2>&1
+    $apktool b -f $apk >/dev/null 2>&1
     mv $apk/dist/$apk.apk "$apk"-optimized.apk
     rm -rf $apk
-    java -jar bin/signapk.jar bin/testkey.x509.pem bin/testkey.pk8 "$apk"-optimized.apk "$apk"-optimized-signed.apk
-    ./bin/zipalign -f 4 "$apk"-optimized-signed.apk "$apk"-optimized-signed-zipaligned.apk
+    $signapk "$apk"-optimized.apk "$apk"-optimized-signed.apk
+    $zipalign -f 4 "$apk"-optimized-signed.apk "$apk"-optimized-signed-zipaligned.apk
 done
 }
 

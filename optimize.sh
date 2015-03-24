0c53c8f61342b82a9b8fdaf3f37dd7829df13d63
Luca Stefani <luca.stefani.ge1@gmail.com>
2015-03-24 13:19:05 +0100

    Speed up decompile process
    * force recompile too

--- optimize.sh
+++ optimize.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 for apk in `ls *.apk | sed "s/.apk//"`; do
-    java -jar bin/apktool.jar d $apk.apk >/dev/null 2>&1
+    java -jar bin/apktool.jar d -s $apk.apk >/dev/null 2>&1
         for png in `find $1 | grep png`; do
 	    ./bin/pngquant $apk/$png >/dev/null 2>&1
         done
-    java -jar bin/apktool.jar b $apk >/dev/null 2>&1
+    java -jar bin/apktool.jar b -f $apk >/dev/null 2>&1
     mv $apk/dist/$apk.apk "$apk"-optimized.apk
     rm -rf $apk
     java -jar bin/signapk.jar bin/testkey.x509.pem bin/testkey.pk8 "$apk"-optimized.apk "$apk"-optimized-signed.apk

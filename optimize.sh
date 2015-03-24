a377c8930adebc12575423860017d3d60488fb1c
Luca Stefani <luca.stefani.ge1@gmail.com>
2015-03-24 13:14:19 +0100

    Fix TESTKEY path

--- optimize.sh
+++ optimize.sh
@@ -8,6 +8,6 @@ for apk in `ls *.apk | sed "s/.apk//"`; do
     java -jar bin/apktool.jar b $apk >/dev/null 2>&1
     mv $apk/dist/$apk.apk "$apk"-optimized.apk
     rm -rf $apk
-    java -jar bin/signapk.jar testkey.x509.pem testkey.pk8 "$apk"-optimized.apk "$apk"-optimized-signed.apk
+    java -jar bin/signapk.jar bin/testkey.x509.pem bin/testkey.pk8 "$apk"-optimized.apk "$apk"-optimized-signed.apk
     ./bin/zipalign -f 4 "$apk"-optimized-signed.apk "$apk"-optimized-signed-zipaligned.apk
 done
